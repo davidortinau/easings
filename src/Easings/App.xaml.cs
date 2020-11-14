c78e38ad07ebfec25616c90d2094273e06fa3176
davidortinau <david.ortinau@microsoft.com>
2020-11-14 08:45:42 -0600

    single and double tap, plays anim in card, navs to editor page stub

--- src/Easings/App.xaml.cs
+++ src/Easings/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Easings
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- src/Easings/Pages/Controls/EasingCard.cs
+++ src/Easings/Pages/Controls/EasingCard.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Shapes;
 
@@ -7,7 +11,7 @@ namespace Easings.Pages.Controls
     {
         public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(EasingCard), string.Empty);
         public static readonly BindableProperty CardDescriptionProperty = BindableProperty.Create(nameof(CardDescription), typeof(string), typeof(EasingCard), string.Empty);
-        // public static readonly BindableProperty EasingPathProperty = BindableProperty.Create(nameof(EasingPath), typeof(Geometry), typeof(EasingCard), string.Empty);
+        public static readonly BindableProperty EasingStyleProperty = BindableProperty.Create(nameof(EasingStyle), typeof(Easing), typeof(EasingCard), Easing.Linear);
         
         public string CardTitle
         {
@@ -22,21 +26,69 @@ namespace Easings.Pages.Controls
         }
         
         
-        // public Geometry EasingPath
-        // {
-        //     get => (Geometry)GetValue(EasingPathProperty);
-        //     set => SetValue(EasingPathProperty, value);
-        // }
+        public Easing EasingStyle
+        {
+            get => (Easing)GetValue(EasingStyleProperty);
+            set => SetValue(EasingStyleProperty, value);
+        }
 
         public EasingCard()
         {
-            this.GestureRecognizers.Add(new TapGestureRecognizer(OnTap));
+            this.GestureRecognizers.Add(new TapGestureRecognizer(OnDblTap){ NumberOfTapsRequired = 2});
+            this.GestureRecognizers.Add(new TapGestureRecognizer(OnTap)
+            {
+                NumberOfTapsRequired = 1
+            });
         }
 
-        private void OnTap(View arg1, object arg2)
+        private void OnDblTap(View arg1, object arg2)
         {
-            var ag = (Grid) this.GetTemplateChild("AxisGrid");
-            ag.IsVisible = (ag.IsVisible) ? false : true;
+            _cancelTapToken.Cancel();
+            Debug.WriteLine("You DOUBLE tapped");
+            Navigation.PushAsync(new EasingEditorPage(), true);
+        }
+
+        private async void OnTap(View arg1, object arg2)
+        {
+            await DebouncedTap().ConfigureAwait(false);
+        }
+
+        private CancellationTokenSource _cancelTapToken = new CancellationTokenSource();
+
+        private async Task DebouncedTap()
+        {
+            try
+            {
+                Interlocked.Exchange(ref _cancelTapToken, new CancellationTokenSource()).Cancel();
+
+                //NOTE THE 500 HERE - WHICH IS THE TIME TO WAIT
+                await Task.Delay(TimeSpan.FromMilliseconds(300), _cancelTapToken.Token)
+
+                    //NOTICE THE "ACTUAL" SEARCH METHOD HERE
+                    .ContinueWith(async task =>
+                        {
+                            Debug.WriteLine("You tapped");
+                            var ag = (Grid) this.GetTemplateChild("AxisGrid");
+                            ag.Opacity = (ag.Opacity == 0) ? 1 : 0;
+
+                            if (ag.Opacity != 0)
+                            {
+                                var b = (BoxView) this.GetTemplateChild("Box");
+                                b.TranslationY = 0;// make sure it's reset
+                                await Task.Delay(500);
+                                await b.TranslateTo(b.TranslationX, ag.Height*-1, 1000, EasingStyle);
+                            }
+                        },
+                        CancellationToken.None,
+                        TaskContinuationOptions.OnlyOnRanToCompletion,
+                        TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            catch
+            {
+                //Ignore any Threading errors
+            }
+            
+            
         }
     }
 }
